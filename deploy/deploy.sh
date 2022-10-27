0e7bde966838f4f524e2c2923d1d699936e3a9ca
Theuns Steyn <theuns7@gmail.com>
2022-10-27 22:17:58 +0000

    Enable the ecs task to be refreshed with new image

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,3 +5,8 @@ sudo docker build -t aws-web -f docker/Dockerfile .
 sudo docker tag aws-web $DOCKER_REPO/aws-web:latest
 aws ecr get-login-password --region us-east-1 | sudo docker login --username AWS --password-stdin $DOCKER_REPO
 sudo docker push $DOCKER_REPO/aws-web:latest
+
+if [ "$1" == "--force" ]; then
+  # Force ecs to take the new docker image
+  aws ecs update-service --cluster aws-web-cluster --service aws-web-service --force-new-deployment
+fi

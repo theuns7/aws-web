355fb9f4d15ccf6741674a2cf405f8c77a86f512
Theuns Steyn <theuns7@gmail.com>
2022-10-27 14:35:43 +0000

    Added task execution role

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -1,5 +1,21 @@
-data "aws_iam_role" "ecs_task_execution_role" {
-  name = "ecsTaskExecutionRole"
+data "aws_iam_policy_document" "task_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "aws_web_ecs_task_execution_role"
+  assume_role_policy = data.aws_iam_policy_document.task_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_pa" {
+  role = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
 resource "aws_ecs_cluster" "cluster" {
@@ -37,7 +53,7 @@ resource "aws_ecs_task_definition" "ecs_task" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = 512
   memory                   = 2048
-  execution_role_arn       = data.aws_iam_role.ecs_task_execution_role.arn
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
   container_definitions    = <<DEFINITION
   [
     {
